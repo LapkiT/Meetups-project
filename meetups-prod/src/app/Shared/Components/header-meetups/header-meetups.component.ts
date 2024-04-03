994bd28fd14632b66c5026329d3127e05b109378
LapkiT <gadzhibekovtimur@gmail.com>
2024-04-03 10:16:09 +0300

    refactoring Pages && Components: swapped

--- meetups-prod/src/app/Shared/Components/header-meetups/header-meetups.component.ts
+++ meetups-prod/src/app/Shared/Components/header-meetups/header-meetups.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-header-meetups',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header-meetups.component.html',
   styleUrl: './header-meetups.component.scss'
--- meetups-prod/src/app/app.routes.ts
+++ meetups-prod/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import {AllMeetupsPegesComponent} from "./Pages/all-meetups-peges/all-meetups-peges.component";
-import {LoginMeetupsComponent} from "./Pages/login-meetups/login-meetups.component";
-import {AddingTasksComponent} from "./Pages/adding-tasks/adding-tasks.component";
+import {LoginMeetupsComponent} from "./Shared/Components/login-meetups/login-meetups.component";
+import {AddingTasksComponent} from "./Shared/Components/adding-tasks/adding-tasks.component";
 
 export const routes: Routes = [
   {path: "allmeetups", component: AllMeetupsPegesComponent},
   {path: "addtask", component: AddingTasksComponent},
-  {path: "login", component: LoginMeetupsComponent}
+  {path: "", component: LoginMeetupsComponent}
 ];
