b871380c23c1dcfbc630415a4b89d4a82337652a
LapkiT <gadzhibekovtimur@gmail.com>
2024-04-05 14:09:58 +0300

    adding interceptor

--- meetups-prod/src/app/Shared/interceptors/jwt.interceptor.ts
+++ meetups-prod/src/app/Shared/interceptors/jwt.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  const token = localStorage.getItem('userToken');
+  const token = localStorage.getItem('jwt_token');
 
   const isUrlCorrect = req.url.startsWith(environment.backendOrigin);
 
