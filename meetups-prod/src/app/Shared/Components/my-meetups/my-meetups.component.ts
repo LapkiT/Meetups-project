a04e131cc62bbea2fa965025565d664791b19b7d
LapkiT <gadzhibekovtimur@gmail.com>
2024-04-06 11:02:05 +0300

    adding my-meetups

---
+++ meetups-prod/src/app/Shared/Components/my-meetups/my-meetups.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-meetups',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-meetups.component.html',
+  styleUrl: './my-meetups.component.scss'
+})
+export class MyMeetupsComponent {
+
+}
